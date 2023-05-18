5def95431252cd5df3fff31e52ec5610207fb1f7
Oleksii <68481433+Sir-Beard@users.noreply.github.com>
2023-05-18 12:29:43 +0300

    Delete "target" dir
    "target" directory is not needed.

--- target/classes/init_db.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE manufacturers
-(
-    id         BIGINT AUTO_INCREMENT
-        PRIMARY KEY,
-    name       VARCHAR(45) NOT NULL,
-    country    VARCHAR(45) NOT NULL,
-    is_deleted TINYINT     NOT NULL DEFAULT 0
-)
-    COMMENT 'List of car makers.';
