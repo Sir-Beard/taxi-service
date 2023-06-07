298cd71c12ae551d9a414e9c56ee09c1591273bd
OleksiiHubanov <oleksiyhubanov@gmail.com>
2023-06-07 23:30:03 +0300

    Taxi service. Task2. Commit #3. 1.Abandoning the idea of reading the file "application.properties" manually in ConnectionUtil.java class (removed "Properties" from the method). 2.Rewrote the annotations @Value so that they worked normally. 3.Cleaning ConnectionUtil.java class. 4.Cleaning AppConfig.java class (removed "Environment environment"). 23:30 07.06.2023

--- src/main/java/com/taxi/config/AppConfig.java
+++ src/main/java/com/taxi/config/AppConfig.java
@@ -3,15 +3,9 @@ package com.taxi.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:application.properties")
 @ComponentScan(basePackages = {"com.taxi"})
 public class AppConfig {
-    private final Environment environment;
-
-    public AppConfig(Environment environment) {
-        this.environment = environment;
-    }
 }
--- src/main/java/com/taxi/util/ConnectionUtil.java
+++ src/main/java/com/taxi/util/ConnectionUtil.java
@@ -1,35 +1,26 @@
 package com.taxi.util;
 
 import com.taxi.exception.DataProcessingException;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ConnectionUtil {
-    @Value("datasource.url")
+    @Value(value = "${datasource.url}")
     private String url;
-    @Value("datasource.username")
+    @Value(value = "${datasource.username}")
     private String name;
-    @Value("datasource.password")
+    @Value(value = "${datasource.password}")
     private String password;
 
     public Connection getConnection() {
-        Connection connection = null;
-        Properties properties = null;
-
+        Connection connection;
         try {
-            properties = new Properties();
-            properties.load(new FileInputStream("src/main/resources/application.properties"));
-            connection = DriverManager.getConnection(properties.getProperty(url),
-                    properties.getProperty(name),
-                    properties.getProperty(password));
-        } catch (SQLException | IOException e) {
+            connection = DriverManager.getConnection(url, name, password);
+        } catch (SQLException e) {
             throw new DataProcessingException("Error occurred while trying connect to db", e);
         }
         return connection;
