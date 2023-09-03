1978ec18f425a1ba58cfd57c4d8ad2a027c1fe2b
OleksiiHubanov <oleksiyhubanov@gmail.com>
2023-09-03 15:45:43 +0300

    Taxi service. Task 6. Commit 4. Maven checkout. timestamp: 15:45 03.09.2023

--- src/main/java/com/taxi/controller/AbstractController.java
+++ src/main/java/com/taxi/controller/AbstractController.java
@@ -1,9 +1,9 @@
 package com.taxi.controller;
 
 import com.taxi.config.AppConfig;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @WebServlet(description = "AnnotationConfigApplicationContext handler for all Controllers")
 public class AbstractController extends HttpServlet {
